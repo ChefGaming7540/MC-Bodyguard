50a804eae85ec8e1de6620e6f1d703f238e614ba
ChefGaming7540 <chefgaming7540@gmail.com>
2025-09-16 00:41:42 +0000

    Rewrote entire bot script; should run flawlessly.

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const readline = require('readline');
 const CONFIG = {
   host: 'ChefsKitchen.aternos.me',
   port: 61563,
-  defaultBotCount: 1,  // Auto-spawn count on startup
-  spawnDelay: 5000,    // Delay between spawns (ms)
+  defaultBotCount: 20,  // Auto-spawn count on startup
+  spawnDelay: 50000,    // Delay between spawns (ms)
   autoRespawn: true,   // Recreate bot if it exits
-  defaultName: 'NamelessKnight'
+  defaultName: 'Chef_Bot'
 };
 
 // === STATE ===
